da69708d9541f97a900736489a0692cbb526cd4d
jiayisunx <jiayi.sun@intel.com>
2026-07-16 21:56:10 +0800

    fix op schema

--- include/sgl_kernel_ops.h
+++ include/sgl_kernel_ops.h
@@ -714,7 +714,7 @@ void embedding_lora_a_fwd(
  * Mamba causal conv1d (XPU)
  */
 void causal_conv1d_fwd(
-    const at::Tensor& x,
+    at::Tensor& x,
     const at::Tensor& weight,
     const std::optional<at::Tensor>& bias_,
     const std::optional<at::Tensor>& conv_states,
@@ -725,8 +725,8 @@ void causal_conv1d_fwd(
     int64_t pad_slot_id);
 
 void causal_conv1d_update(
-    const at::Tensor& x,
-    const at::Tensor& conv_state,
+    at::Tensor& x,
+    at::Tensor& conv_state,
     const at::Tensor& weight,
     const std::optional<at::Tensor>& bias_,
     bool silu_activation,
